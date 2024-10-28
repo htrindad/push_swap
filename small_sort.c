b177ba6a0917d746700903a2ae34d4b8427fb731
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2024-10-28 19:37:07 +0000

    optimization

--- small_sort.c
+++ small_sort.c
@@ -6,51 +6,30 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 18:27:44 by htrindad          #+#    #+#             */
-/*   Updated: 2024/10/27 18:47:52 by htrindad         ###   ########.fr       */
+/*   Updated: 2024/10/28 19:30:16 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	sorting(t_stack **a, int *val, int highest)
+static void	sorting(t_stack **a, int highest)
 {
-	if (highest == val[0])
-	{
+	if ((*a)->val == highest)
 		ra(a);
-		if (val[1] > val[2])
-			sa(a);
-	}
-	else if (highest == val[1])
-	{
-		if (val[0] > val[2])
-		{
-			rra(a);
-			sa(a);
-		}
-		else if (val[0] < val[2])
-		{
-			sa(a);
-			ra(a);
-		}
-	}
-	else if (highest == val[2] && val[1] < val[0])
+	else if ((*a)->next->val == highest)
+		rra(a);
+	if ((*a)->val > (*a)->next->val)
 		sa(a);
 }
 
 void	small_sort(t_stack **a, int highest)
 {
-	int	*val;
-
 	if (!ps_sorted(*a))
 	{
 		if (ps_countnode(*a) == 2)
 			sa(a);
 		else
-		{
-			val = get_vals(*a);
-			sorting(a, val, highest);
-			free(val);
-		}
+			sorting(a, highest);
 	}
 }
 
