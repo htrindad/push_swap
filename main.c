81d773195f0e598a545776878dfc101c713f632e
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2024-11-10 16:07:22 +0000

    repeat

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 13:06:31 by htrindad          #+#    #+#             */
-/*   Updated: 2024/11/10 13:34:27 by htrindad         ###   ########.fr       */
+/*   Updated: 2024/11/10 16:06:44 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,11 @@ int	main(int ac, char **av)
 	if (ac == 2)
 		av = ps_split(av[1], ' ');
 	ps_init(&a, av + 1, ac == 2);
+	if (ps_duplicate(a))
+	{
+		ps_freestack(&a);
+		ps_quiterror();
+	}
 	if (!ps_sorted(a))
 	{
 		if (ps_countnode(a) == 2)
